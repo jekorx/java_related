406b5c33b2a4ef2e374bb86137a6b797b4c8d763
jekorx <wdg404@163.com>
2018-02-07 11:18:25 +0800

    rearrange

--- _basic_framework/springboot/src/test/java/com/spring/boot/TestMain.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot;
-
-public class TestMain {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- _basic_framework/ssm/README.md
+++
@@ -1,3 +0,0 @@
-# ssm
-Spring+SpringMVC+MyBatis+MySQL基础搭建
-带有分页插件js版和jsp标签版
